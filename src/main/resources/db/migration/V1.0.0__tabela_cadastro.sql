d64449e31ffe302f7e07ef13ae64212f9e173ac6
WallissonMarinho <wallissonmarinho@hotmail.com>
2020-02-18 07:55:08 -0300

    migration alterado

--- src/main/resources/db/migration/V1.0.0__tabela_cadastro.sql
+++ src/main/resources/db/migration/V1.0.0__tabela_cadastro.sql
@@ -11,6 +11,6 @@ CREATE TABLE cadastro (
 
 INSERT INTO cadastro (nome, descricao, viabilidade, situacao, data_cadastro, data_situacao) values ('Fulano 1', 'descrição da ideia', '5', 'DESENVOLVIMENTO', '2020-02-09', null);
 INSERT INTO cadastro (nome, descricao, viabilidade, situacao, data_cadastro, data_situacao) values ('Fulano 2', 'descrição da ideia', '4', 'REGISTRADA', '2020-02-08', null);
-INSERT INTO cadastro (nome, descricao, viabilidade, situacao, data_cadastro, data_situacao) values ('Fulano 3', 'descrição da ideia', '3', 'CANCELADA', '2020-02-07', null);
-INSERT INTO cadastro (nome, descricao, viabilidade, situacao, data_cadastro, data_situacao) values ('Fulano 4', 'descrição da ideia', '2', 'DESENVOLVIDA', '2020-02-06', null);
+INSERT INTO cadastro (nome, descricao, viabilidade, situacao, data_cadastro, data_situacao) values ('Fulano 3', 'descrição da ideia', '3', 'CANCELADA', '2020-02-07', '2020-02-08');
+INSERT INTO cadastro (nome, descricao, viabilidade, situacao, data_cadastro, data_situacao) values ('Fulano 4', 'descrição da ideia', '2', 'DESENVOLVIDA', '2020-02-06', '2020-02-08');
 INSERT INTO cadastro (nome, descricao, viabilidade, situacao, data_cadastro, data_situacao) values ('Fulano 5', 'descrição da ideia', '1', 'CANCELADA', '2020-02-05', null);
